9fe756e24185883fe95f2ed8e6fdfc7bce6f88aa
piyush <piyushujgaokar544@gmail.com>
2026-03-29 16:42:24 +0530

    Implemented The RAG

--- src/app.js
+++ src/app.js
@@ -1,25 +1,73 @@
 import express from 'express'
-import {PDFParse} from 'pdf-parse'
+import { PDFParse} from 'pdf-parse'
 import fs from 'fs'
+import dotenv from 'dotenv'
+dotenv.config()
+import { Pinecone } from '@pinecone-database/pinecone'
+import { MistralAIEmbeddings } from "@langchain/mistralai";
 import {RecursiveCharacterTextSplitter} from '@langchain/textsplitters'
+import { log } from 'console'
 
 const app=express()
 
-    let dataBuffer=fs.readFileSync('./Full Stack 2.pdf')
-    const parser=new PDFParse({
-        data:dataBuffer
-    })
-    const data=await parser.getText()
-    console.log(data)
 
-
-
-const textSplitter=new RecursiveCharacterTextSplitter({
-    chunkSize:300,
-    chunkOverlap:0
+const pc= new Pinecone({
+    apiKey:process.env.PINECONE_API_KEY,
 })
 
-const chunks=await textSplitter.splitText(data.text)
-console.log(chunks,chunks.length)
+    const index=pc.index('rag')
+
+    // let dataBuffer=fs.readFileSync('./Full Stack 2.pdf')
+    // const parser=new PDFParse({
+    //     data:dataBuffer
+    // })
+    // const data=await parser.getText()
+
+    const mistralEmbeddings=new MistralAIEmbeddings({
+        apiKey:process.env.MISTRAL_API_KEY,
+        model:"mistral-embed"
+    })
+
+// const textSplitter=new RecursiveCharacterTextSplitter({
+//     chunkSize:300,
+//     chunkOverlap:0
+// })
+
+// const chunks=await textSplitter.splitText(data.text)
+//     console.log(chunks)
+ 
+//     const docs=await Promise.all(chunks.map(async (chunk)=>{
+//         const embedding=await mistralEmbeddings.embedQuery(chunk)
+//         return {
+//             text:chunk,
+//             embedding
+//         }
+//     }))
+
+//    const result=await index.upsert({
+//     records:docs.map((doc,i)=>({
+//         id:`doc-${i}`,
+//         values:doc.embedding,
+//         metadata:{
+//             text:doc.text 
+//         }
+//     }))
+//    })
+//    console.log(result)
+
+
+    const queryEmbedding=await mistralEmbeddings.embedQuery("currently piyush is studying in which college? ")
+
+    console.log(queryEmbedding)
+
+    const result=await index.query({
+        topK:2,
+        vector:queryEmbedding,
+        includeMetadata:true
+    })
+    
+    const resultData=JSON.stringify(result)
+    console.log(resultData)
+
 
 export default app
